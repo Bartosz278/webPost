189ec4a557e572026fcc883f3570fff168d36d06
nerievv <queker001@gmail.com>
2022-04-10 12:41:16 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const textField = document.getElementById("textField");
+let copyCache = undefined;
+
+function openNav() {
+    document.getElementById("mContent").style.marginTop = "200px"
+    document.getElementById("topNav").style.height = "200px";
+}
+
+function closeNav() {
+    document.getElementById("topNav").style.height = "0px;";
+    document.getElementById("mContent").style.marginTop = "0px";
+}
+TODO:
+
+    function copyText() {
+        document.getElementById("innerOutput").value = textField.innerText;
+    }
+    // document.getElementById("sendButton").dadEventListener("click", copyText);
