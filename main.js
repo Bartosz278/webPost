b47345075e875ca1a7ab4cd38a7c7ba5396c7b9a
nerievv <queker001@gmail.com>
2022-05-29 15:28:51 +0200

    a

--- main.js
+++ main.js
@@ -6,23 +6,3 @@ burger.addEventListener('click', function(e) {
     this.classList.toggle('is-open');
     nav.classList.toggle('is-open');
 });
-
-slowmo.addEventListener('click', function(e) {
-    this.classList.toggle('is-slowmo');
-});
-
-/* Onload demo - dirty timeout */
-let clickEvent = new Event('click');
-
-window.addEventListener('load', function(e) {
-    slowmo.dispatchEvent(clickEvent);
-    burger.dispatchEvent(clickEvent);
-
-    setTimeout(function() {
-        burger.dispatchEvent(clickEvent);
-
-        setTimeout(function() {
-            slowmo.dispatchEvent(clickEvent);
-        }, 3500);
-    }, 5500);
-});
