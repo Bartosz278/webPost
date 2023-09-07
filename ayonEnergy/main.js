fe3ab77a9f454c60f8c5e11c653b3ba433d54c2a
Bartosz <queker001@gmail.com>
2023-09-07 16:19:31 +0200

    delete files change paths add opening links by click main sections

---
+++ ayonEnergy/main.js
@@ -0,0 +1,8 @@
+let burger = document.getElementById('burger'),
+    nav = document.getElementById('main-nav'),
+    slowmo = document.getElementById('slowmo');
+
+burger.addEventListener('click', function(e) {
+    this.classList.toggle('is-open');
+    nav.classList.toggle('is-open');
+});
--- main.js
+++
@@ -1,8 +0,0 @@
-let burger = document.getElementById('burger'),
-    nav = document.getElementById('main-nav'),
-    slowmo = document.getElementById('slowmo');
-
-burger.addEventListener('click', function(e) {
-    this.classList.toggle('is-open');
-    nav.classList.toggle('is-open');
-});
